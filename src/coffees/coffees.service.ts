cc40e75f1efca19f1e4cd99ad72546bace131540
carlosrodrigues94 <carlos.1994hrs@gmail.com>
2021-06-26 15:53:59 -0300

    chore: dependency injection examples

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Event } from 'src/events/entities/event.entity';
 import { Connection, Repository } from 'typeorm';
+import { COFFEE_BRANDS } from './coffees.constants';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
@@ -17,7 +18,11 @@ export class CoffeesService {
     private readonly flavorRepository: Repository<Flavor>,
 
     private readonly connection: Connection,
-  ) {}
+
+    @Inject(COFFEE_BRANDS) private readonly coffeeBrands: string[],
+  ) {
+    console.log(this.coffeeBrands);
+  }
 
   async findAll({ limit, offset }: PaginationQueryDto) {
     const coffees = await this.coffeeRepository.find({
